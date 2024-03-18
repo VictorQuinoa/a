750a1626b4fd184d849fb05469c4fdb5427b1a04
VictorQuinoa <145323734+VictorQuinoa@users.noreply.github.com>
2024-03-18 18:51:34 +0100

    Boletin 19

---
+++ Boletin19/src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConversorTemperaturas conversor = new ConversorTemperaturas();
+        boolean temp = false;
+
+        while (!temp) {
+            try {
+                float temperatura= Float.parseFloat(JOptionPane.showInputDialog("Introduzca la Temperatura"));
+
+                float fahrenheit = ConversorTemperaturas.centigradosAFharenheit(temperatura); // Corregido el nombre del método
+                System.out.println("Temperatura en Fahrenheit: " + fahrenheit);
+
+                float reamur = ConversorTemperaturas.centigradosAReamur(temperatura);
+                System.out.println("Temperatura en Reamur: " + reamur);
+                temp = true;
+            } catch (TemperaturaErradaExcepcion e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error inesperado: " + e.getMessage());
+
+            }
+        }
+    }
+
+}
---
+++ Boletin19/src/TemperaturaErradaExcepcion.java
@@ -0,0 +1,11 @@
+
+
+public class TemperaturaErradaExcepcion extends Exception {
+
+    public TemperaturaErradaExcepcion(String mensaje) {
+        super(mensaje);
+////
+    }
+
+}
+
